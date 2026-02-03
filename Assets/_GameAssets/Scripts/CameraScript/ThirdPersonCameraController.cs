46e16d414bb3b3aca6a2623ead35976546fc501d
Egeli4588 <ali_uysun45@hotmail.com>
2026-02-03 15:05:28 +0300

    yapayZeka yön  problemini çözdü

--- Assets/_GameAssets/Scripts/CameraScript/ThirdPersonCameraController.cs
+++ Assets/_GameAssets/Scripts/CameraScript/ThirdPersonCameraController.cs
@@ -12,12 +12,13 @@ public class ThirdPersonCameraController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 viewDirection = new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
+        Vector3 viewDirection = _playerTransform.position - transform.position;
+        viewDirection.y = 0f;
         _orientationTransform.forward = viewDirection.normalized;
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 inputDirection = _orientationTransform.forward * verticalInput + _orientationTransform.right * verticalInput;
+        Vector3 inputDirection = _orientationTransform.forward * verticalInput + _orientationTransform.right * horizontalInput;
         if (inputDirection!=Vector3.zero)
         {
             _playerVisualTransform.forward = Vector3.Slerp(_playerVisualTransform.forward, inputDirection.normalized, _rotationSpeed * Time.deltaTime);
